ad1c09dace50d8f7765cfc97e143c0216ad183b0
JoaoFontanella <joaofontanella31@gmail.com>
2025-09-23 20:47:20 -0300

    Teste 04

--- test/api_prova.spec.ts
+++ test/api_prova.spec.ts
@@ -2,29 +2,47 @@ import pactum from 'pactum';
 import { StatusCodes } from 'http-status-codes';
 import { SimpleReporter } from '../simple-reporter';
 
-describe('Teste POST de Produtos', () => {
+describe('Teste GET de Objetos', () => {
   const p = pactum;
   const rep = SimpleReporter;
-  const baseUrl = 'https://fakestoreapi.com';
+  const baseUrl = 'https://api.restful-api.dev';
 
   p.request.setDefaultTimeout(30000);
 
   beforeAll(() => p.reporter.add(rep));
   afterAll(() => p.reporter.end());
 
-  it('ADD um produto no carrinho', async () => {
-    const newProduct = {
-      title: 'Caneca',
-      price: 29.99,
-      description: 'Caneca de cerâmica',
-      image: 'https://teste.com',
-      category: 'Cozinha'
+  it('Listar todos os objetos', async () => {
+    await p
+      .spec()
+      .get(`${baseUrl}/objects`)
+      .expectStatus(StatusCodes.OK)
+  });
+});
+
+describe('Teste POST de Objetos', () => {
+  const p = pactum;
+  const rep = SimpleReporter;
+  const baseUrl = 'https://api.restful-api.dev';
+
+  p.request.setDefaultTimeout(30000);
+
+  beforeAll(() => p.reporter.add(rep));
+  afterAll(() => p.reporter.end());
+
+  it('Adicionar um novo objeto', async () => {
+    const newObject = {
+      name: 'Smartphone XYZ',
+      data: {
+        price: 499.99,
+        color: 'Black',
+      },
     };
 
     await p
       .spec()
-      .post(`${baseUrl}/products`)
-      .withJson(newProduct)
-      .expectStatus(StatusCodes.CREATED)
+      .post(`${baseUrl}/objects`)
+      .withJson(newObject)
+      .expectStatus(StatusCodes.OK)
   });
 });
